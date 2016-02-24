cb1d9cf771fd38bb7085248b6623f7a71e4f1da2
Colin Gourlay <colin@colin-gourlay.com>
2016-02-24 20:40:12 +1000

    1.0.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# November Nineteenth
+
+A twitter bot that replies to anyone who isn't aware that there is, in fact, an International Men's Day.
+
+## Why?
+
+The comedian Richard Herring has been [doing this job manually](http://www.telegraph.co.uk/comedy/comedians/richard-herring-spends-international-womens-day-fighting-sexism/) every International Women's Day (because for some _unknown reason_, this is when people like to display their ignorance) for a while, but one human being at a keyboard can only do so much, so this little script thought it would help out a bit.
+
+## How?
+
+Clone this repo.
+
+Copy `config.example.js` to `config.js` and fill out the fields with tokens/keys you can create [here](https://apps.twitter.com/).
+
+Then, in your terminal, `cd` into the project directory and run:
+
+    npm install
+    node index.js
---
+++ index.js
@@ -0,0 +1,74 @@
+var Twitter = require('twitter');
+var config = require('./config');
+
+var PHRASE = 'international men\'s day';
+var PHRASE_REGEX = new RegExp(PHRASE);
+var QUESTIONS_REGEX = /what|when|why|how/;
+var IGNORE_REGEX = /november|19/;
+var ONE_DAY= 1000 * 60 * 60 * 24;
+var REPLIES = [
+    'Hey {{name}}, International Men\'s Day is November 19th.',
+    'International Men\'s Day is November 19th. Only {{days}} sleeps to go! 💤',
+    'I have great news for you, {{name}}. International Men\'s Day is November 19th.',
+    'On November 19th all your dreams will come true. It\'ll be International Men\'s day! 👬',
+    'You only have to wait {{days}} days! International Men\'s Day is November 19th.',
+    'International Men\'s Day is November 19th, only {{days}} days away! Get excited, {{name}}!!!',
+    'Clear your schedule on November 19th, {{name}}. It\'s International Men\'s Day, and we\'re gonna party! 🎉'
+];
+var NOOP = function () {};
+
+var twitter = new Twitter(config);
+var nextReply = 0;
+
+function onStream(stream) {
+    stream.on('data', onTweet);
+    stream.on('error', onError);
+}
+
+function onTweet(tweet) {
+    var text = tweet.text.toLowerCase();
+
+    if (text.match(PHRASE_REGEX) === null ||
+        text.match(QUESTIONS_REGEX) === null ||
+        text.match(IGNORE_REGEX) !== null) {
+        return;
+    }
+
+    console.log('Query from @' + tweet.user.screen_name + ': "' + tweet.text + '"');
+
+    reply(tweet);
+}
+
+function reply(tweet) {
+    var status = REPLIES[nextReply++]
+        .replace('{{name}}', tweet.user.name.split(' ')[0])
+        .replace('{{days}}', days());
+
+    if (nextReply >= REPLIES.length) {
+        nextReply = 0;
+    }
+
+    console.log('Replying with: "' + status + '"');
+
+    twitter.post('statuses/update', {
+        in_reply_to_status_id: tweet.id,
+        status: '@' + tweet.user.screen_name + ' ' + status
+    }, NOOP);
+}
+
+function days() {
+    var today = new Date();
+    var imd = new Date(today.getFullYear(), 10, 19);
+
+    if (today.getMonth() == 10 && today.getDate() > 19 || today.getMonth() > 10) {
+        imd.setFullYear(imd.getFullYear() + 1);
+    }
+
+    return Math.ceil((imd.getTime() - today.getTime()) / (ONE_DAY));
+}
+
+function onError(error) {
+    throw error;
+}
+
+twitter.stream('statuses/filter', {track: PHRASE}, onStream);
