32215fe9547e4aeebe83608c53f315b68e9d9b44
Colin Gourlay <colin@colin-gourlay.com>
2016-03-06 09:35:36 +1000

    Ignore retweets. Updated false positives. Respond to false positives from a different list of replies.

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var config = require('./config');
 var PHRASE = 'international men\'s day';
 var PHRASE_REGEX = new RegExp(PHRASE);
 var QUESTIONS_REGEX = /what|when|why|how/;
-var IGNORE_REGEX = /november|19|say/;
-var ONE_DAY= 1000 * 60 * 60 * 24;
-var REPLIES = [
+var FALSE_POSITIVES_REGEX = /guy|bro|dude|who|ask|say|nov|19/;
+var QUESTION_REPLIES = [
     'Hey {{name}}, International Men\'s Day is November 19th.',
     'International Men\'s Day is November 19th. Only {{days}} sleeps to go! 💤',
     'I have great news for you, {{name}}. International Men\'s Day is November 19th.',
@@ -15,10 +14,18 @@ var REPLIES = [
     'International Men\'s Day is November 19th, only {{days}} days away! Get excited, {{name}}!!!',
     'Clear your schedule on November 19th, {{name}}. It\'s International Men\'s Day, and we\'re gonna party! 🎉'
 ];
+var FALSE_POSITIVE_REPLIES = [
+    'I try to tell as many people as I can, but it\'s a lot of work.',
+    'It\'s not easy, {{name}}, but someone\'s gotta spread the word.',
+    'Tell me about it, {{name}}.',
+    'The best I can do is try to reply to as many people with the date.'
+];
+var ONE_DAY= 1000 * 60 * 60 * 24;
 var NOOP = function () {};
 
 var twitter = new Twitter(config);
-var nextReply = 0;
+var nextQuestionReplyIndex = 0;
+var nextFalsePositiveReplyIndex = 0;
 
 function onStream(stream) {
     stream.on('data', onTweet);
@@ -30,24 +37,42 @@ function onTweet(tweet) {
 
     if (text.match(PHRASE_REGEX) === null ||
         text.match(QUESTIONS_REGEX) === null ||
-        text.match(IGNORE_REGEX) !== null) {
+        tweet.retweeted_status != null) {
+
         return;
+
     }
 
-    console.log('Query from @' + tweet.user.screen_name + ': "' + tweet.text + '"');
+    if (text.match(FALSE_POSITIVES_REGEX) === null) {
 
-    setTimeout(reply, 60000, tweet);
+        setTimeout(reply, 60000, tweet, false);
+
+    } else {
+
+        setTimeout(reply, 60000, tweet, true);
+
+    }
+
 }
 
-function reply(tweet) {
-    var status = REPLIES[nextReply++]
+function reply(tweet, isFalsePositive) {
+    var status = (isFalsePositive ?
+        FALSE_POSITIVE_REPLIES[nextFalsePositiveReplyIndex++] :
+        QUESTION_REPLIES[nextQuestionReplyIndex++])
         .replace('{{name}}', tweet.user.name.split(' ')[0])
         .replace('{{days}}', days());
 
-    if (nextReply >= REPLIES.length) {
-        nextReply = 0;
+    if (nextFalsePositiveReplyIndex >= FALSE_POSITIVE_REPLIES.length) {
+        nextFalsePositiveReplyIndex = 0;
     }
 
+    if (nextQuestionReplyIndex >= QUESTION_REPLIES.length) {
+        nextQuestionReplyIndex = 0;
+    }
+
+    console.log(tweet);
+
+    console.log('Tweet from @' + tweet.user.screen_name + ': "' + tweet.text + '"');
     console.log('Replying with: "' + status + '"');
 
     twitter.post('statuses/update', {
